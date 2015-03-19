c804a9dd583b0c93bed62d21258fc42719e2942e
Nicolas Bertrand <nicoinattendu@gmail.com>
2015-03-19 13:01:26 +0100

    add lftp feature

--- exec-done.py
+++ exec-done.py
@@ -17,9 +17,57 @@ import shutil
 from smtplib import SMTP as SMTP       # this invokes the secure SMTP protocol (port 465, uses SSL)
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+import subprocess as SP
 import json
 
-CONFIG_FILE="/etc/transmission-daemon/exec-done.json"
+#CONFIG_FILE="/etc/transmission-daemon/exec-done.json"
+CONFIG_FILE="./exec-done.json"
+
+class TdcpbException(Exception):
+    def __init__(self, message, errors = None):
+        Exception.__init__(self, message)
+
+class Lftp(object):
+    LFTP_CMDS="set ftp:ssl-force true; set ssl:verify-certificate no; set xfer:log true; mirror --verbose=3 -R {} ; quit"
+
+    def __init__(self, p_dir_path, p_config_data) :
+        self.config_data = p_config_data
+        # TODO verfiy p_dir_path
+        self.dir_path = p_dir_path
+
+        lftp_cmd = self.LFTP_CMDS.format( self.dir_path )
+        _ftp_connect="ftp://{}:{}@{}".format(self.config_data['ftp-user'],
+                                             self.config_data['ftp-pass'],
+                                             self.config_data['ftp-host'])
+        self.cmd = ["lftp",
+            _ftp_connect,
+            "-e",
+            lftp_cmd
+            ]
+        logging.debug("Cmd: {}".format(" ".join(self.cmd)))
+
+
+    def mirror(self):
+        logging.info("Starting FTP copy of {}".format(os.path.basename(self.dir_path)))
+        try :
+            self.run_lftp()
+        except TdcpbException as _err:
+            logging.error("Copy of {} FAILED".format(os.path.basename(self.dir_path)))
+        else:
+            logging.info("Copy of {} successfull".format(os.path.basename(self.dir_path)))
+
+    def run_lftp(self):
+        sync = SP.Popen(self.cmd, stdout=SP.PIPE, stderr= SP.PIPE)
+        (stdout, stderr) = sync.communicate()
+        logging.debug( stdout)
+        if sync.returncode:
+            _msg = "FTP command failed"
+            logging.error(_msg)
+            logging.error(stderr)
+            raise TdcpbException(_msg)
+
+
+
 
 def sendMail(p_torrent_msg, p_config_data) :
     _subject = "Reception de {} sur {}".format(p_torrent_msg["name"], p_torrent_msg["hostname"])
@@ -105,16 +153,16 @@ def parseConfig(p_config_file):
 
 def main(argv):
     config_data = parseConfig(CONFIG_FILE)
-    logging.basicConfig(format = '%(asctime)s %(levelname)s %(message)s # %(filename)s %(funcName)s l %(lineno)d', level=logging.ERROR)
+    logging.basicConfig(format = '%(asctime)s %(levelname)s %(message)s # %(filename)s %(funcName)s l %(lineno)d', level=logging.DEBUG)
 
     torrent_msg ={}
-    torrent_msg["name"] = os.environ.get('TR_TORRENT_NAME',  "ERROR_NO_NAME")
-    torrent_msg["time"] = os.environ.get('TR_TIME_LOCALTIME',"ERROR_NO_TIME")
-    torrent_msg["dir"]  = os.environ.get('TR_TORRENT_DIR',   "ERROR_NO_DIR")
-    torrent_msg["hash"] = os.environ.get('TR_TORRENT_HASH',  "ERROR_NO_HASH")
-    torrent_msg["id"]   = os.environ.get('TR_TORRENT_ID',    "ERROR_NO_ID")
-    torrent_msg["ip"]   = get_tinc_ip()
-    torrent_msg["hostname"]   = get_hostname()
+    torrent_msg["name"]     = os.environ.get('TR_TORRENT_NAME',  "ERROR_NO_NAME")
+    torrent_msg["time"]     = os.environ.get('TR_TIME_LOCALTIME',"ERROR_NO_TIME")
+    torrent_msg["dir"]      = os.environ.get('TR_TORRENT_DIR',   "ERROR_NO_DIR")
+    torrent_msg["hash"]     = os.environ.get('TR_TORRENT_HASH',  "ERROR_NO_HASH")
+    torrent_msg["id"]       = os.environ.get('TR_TORRENT_ID',    "ERROR_NO_ID")
+    torrent_msg["ip"]       = get_tinc_ip()
+    torrent_msg["hostname"] = get_hostname()
     mycontent = []
     mycontent.append("DCP               : {}\n".format(torrent_msg["name"]) )
     mycontent.append("Date de Reception : {}\n".format(torrent_msg["time"]) )
@@ -126,6 +174,11 @@ def main(argv):
 
     #mycontent.append("Date de Generation: %s\n"%(time.strftime("%c")))
     WriteFile("/tmp/tdcpb-{}.log".format(torrent_msg["name"]), mycontent)
+    if config_data['ftp-library'] :
+        print "FTP vers la librairie"
+        _dir_path = os.path.join(torrent_msg["dir"],torrent_msg["name"])
+        ftp = Lftp(_dir_path, config_data)
+        ftp.mirror()
     sendMail(torrent_msg, config_data)
 
 
