3e83b06ed825e3102a897480f70a34abc336ce4f
Nicolas Bertrand <nicolas.bertrand@isf.cc>
2016-10-10 17:45:03 +0200

    set version 0.51

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description  = "colletion of toools for tdcpb: ingets, verify, transmission post exec",
     author       = "Nicolas Bertrand",
     author_email = "nicolas@indecp.org",
-    version      = "0.5",
+    version      = "0.51",
     scripts      = [
         "bin/tdcpbftp",
         "bin/tdcpb-checkdcp-short",
